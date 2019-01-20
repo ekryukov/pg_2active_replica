b74de77525b43a5e60786d5caeca098fdb0a3f46
Evgeny Krukov <ekryukov@lanit.ru>
2019-01-20 12:57:36 +0300

    update postgres settings

--- provisioning/roles/postgres94/files/init_bdr.sql
+++ provisioning/roles/postgres94/files/init_bdr.sql
@@ -8,7 +8,13 @@ SELECT bdr.bdr_group_create(
 SELECT bdr.bdr_node_join_wait_for_ready();
 
 SELECT bdr.bdr_group_join(
-    local_node_name := 'node2',
+    local_node_name := 'node3',
     node_external_dsn := 'dbname=svip_db host=10.8.110.12',
     join_using_dsn := 'dbname=svip_db host=10.8.110.21'
 );
+
+SELECT bdr.internal_begin_join(
+        'bdr_group_join',
+        local_node_name,
+        CASE WHEN node_local_dsn IS NULL THEN node_external_dsn ELSE node_local_dsn END,
+        join_using_dsn);
